0c13db50aaec204767ef91f791725301119cfc9c
qiezis <1007016771@qq.com>
2017-12-14 17:38:27 +0800

    indexPage

--- src/main.js
+++ src/main.js
@@ -1,59 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-
-// import router from './router'
-import VRouter from 'vue-router'
-import Vuex from 'vuex'
-
-//组件
-import Apple from './components/Apple.vue'
-import Banana from './components/Banana'
-import redApple from './components/redApple'
-import  yellowApple from './components/yellowApple.vue'
-
-Vue.use(VRouter)
-Vue.use(Vuex)
-let store=new Vuex.Store({
-  state:{
-    totalPrice:0
-  },
-  mutations:{
-    plusPrice(state,price){
-      state.totalPrice += price
-    },
-    reducePrice(state,price){
-      state.totalPrice -= price
+import Layout from './components/layout.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import indexPage from './pages/index'
+let router=new VueRouter({
+  mode:'history',
+  routes:[
+    {
+      path:'/',
+      component:indexPage
     }
-  },
+  ]
 })
-
-
-// let router=new VRouter({
-//     mode:'history',
-//     routes:[
-//       {
-//         path:'/apple',
-//         component:Apple,
-//         children:[
-//           {
-//             path:'red',
-//             component:redApple,
-//           },
-//           {
-//             path:'yellow',
-//             component:yellowApple,
-//           }
-//         ]
-//       },
-//       {
-//         path:'/banana',
-//         component:Banana
-//       }
-//     ]
-// })
-
 
 
 
@@ -62,7 +22,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  store,
-  template: '<App/>',
-  components: { App }
+  router,
+  template: '<Layout/>',
+  components: { Layout }
 })
